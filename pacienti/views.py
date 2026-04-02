d7681d04921923df26b8b21794d873bf85a80be8
Sorin Hus <sorin.hus@gmail.com>
2026-04-02 12:33:12 +0300

    fix programari

--- pacienti/views.py
+++ pacienti/views.py
@@ -1,5 +1,5 @@
 from rest_framework import viewsets, permissions, filters
-from rest_framework.decorators import action
+from rest_framework.decorators import action, api_view
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.filters import SearchFilter
