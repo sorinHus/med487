fb2f5998d7c730e6d7ebf0babe3e639b55e72785
Sorin Hus <sorin.hus@gmail.com>
2026-04-06 10:15:31 +0300

    migration 0015

--- pacienti/migrations/0015_pacient_user.py
+++ pacienti/migrations/0015_pacient_user.py
@@ -1,16 +1,24 @@
+import django.db.models.deletion
+from django.conf import settings
 from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('pacienti', '0015_auto_20250101_0000'),
+        ('pacienti', '0014_customuser_aprobat'),
     ]
 
     operations = [
         migrations.AddField(
             model_name='pacient',
-            name='nume_anterior',
-            field=models.CharField(blank=True, max_length=100, null=True, verbose_name='Nume anterior'),
+            name='user',
+            field=models.OneToOneField(
+                blank=True,
+                null=True,
+                on_delete=django.db.models.deletion.SET_NULL,
+                related_name='pacient_profil',
+                to=settings.AUTH_USER_MODEL,
+            ),
         ),
     ]
